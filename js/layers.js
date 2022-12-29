8628d1fb4da06ad1ba00b5b6b436c3894a05bbe4
Glitch (elite-caterwauling-tomato) <none>
2022-12-29 07:41:58 +0000

    🚶👮 Checkpoint ./js/layers.js:34799145/29:35087712/207

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,4 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+addLayer("p",{
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -14,11 +13,12 @@ addLayer("p", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+        let mult = new Decimal(1)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
+        let mult = new Decimal(1)
+        return mult
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
